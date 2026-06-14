fb6d72716363e7a8e56145f49f747060434490df
ulcelluletifo <ulcelluletifo@gmail.com>
2026-06-15 00:08:57 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 
 const UL_CONFIG = {
   SUPABASE_URL: 'https://ypsvdicatkckrxrcyxax.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_Wq8Megzoj3bKeDgOx1Pw2g_gBYaz...',
+  SUPABASE_ANON_KEY: 'sb_publishable_Wq8Megzoj3bKeDgOx1Pw2g_gBYazJ89.',
   APP_VERSION: '1.0.0',
   SAISON_COURANTE: '2026-2027',
 };
