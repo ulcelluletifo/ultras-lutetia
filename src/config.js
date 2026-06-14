c02c6a9ba5156a930b9c48d52b1f650cd34f667d
ulcelluletifo <ulcelluletifo@gmail.com>
2026-06-15 00:15:29 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 
 const UL_CONFIG = {
   SUPABASE_URL: 'https://ypsvdicatkckrxrcyxax.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_Wq8Megzoj3bKeDgOx1Pw2g_gBYazJ89.',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwc3ZkaWNhdGtja3J4cmN5eGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjI5NjUsImV4cCI6MjA5NzAzODk2NX0.qDWSmYYCB6_95rnwqXjOYqizMFG2VOfUmifw_h6bEmU',
   APP_VERSION: '1.0.0',
   SAISON_COURANTE: '2026-2027',
 };
