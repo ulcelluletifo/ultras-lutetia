cb82fb537616e30bb2b6dc1bae661f29f3305c52
ulcelluletifo <ulcelluletifo@gmail.com>
2026-06-15 08:38:43 +0200

    Update supabase-client.js

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -6,8 +6,6 @@
 const SUPABASE_URL = 'https://ypsvdicatkckrxrcyxax.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwc3ZkaWNhdGtja3J4cmN5eGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjI5NjUsImV4cCI6MjA5NzAzODk2NX0.qDWSmYYCB6_95rnwqXjOYqizMFG2VOfUmifw_h6bEmU';
 
-
-
 // ── Init client Supabase ──────────────────────────────────────
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
